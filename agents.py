30e6f4eb361b518028a94d6160d45529bdcbd2b4
Gregg <gregg@unanimous.ai>
2020-01-02 09:38:56 -0600

    Telephone network works!

--- agents.py
+++ agents.py
@@ -16,47 +16,69 @@ class Agent:
     A simple linear agent class. Can be used for output agents, and is extended below.
     """
 
-    def __init__(self, n_connections_in):
+    def __init__(self, agent_id, n_connections_in):
         """
         Creates a linear agent. This agent has:
          -n unit input connections
          -a 100-long history of inputs and outputs
         """
+        # Records this agent's ID
+        self.id = agent_id
+
         # initializes the input history with a fit of 1... just to start the model off.
         self.connections = np.ones(n_connections_in)
-        self.bias = 0
+
+        # Tracks the recent connections to measure change in connection importance
+        self.last_importance = np.zeros(self.connections.shape)
+
+        # Tracking variables for this agent's history
+        self.last_inputs = np.zeros(n_connections_in)
         self.input_history = [np.zeros(n_connections_in), np.ones(n_connections_in)/n_connections_in]
         self.utility_history = [0, 1]
         self.history_length = 1000
 
     def signal(self, input):
         """
-        Processes a set of inputs into a single signal output
+        Processes a set of inputs into a single signal output. Records this last input
         """
-        return np.sum(self.connections*input) + self.bias
+        self.last_inputs = input
+        return np.sum(self.connections*np.asarray(input))
 
-    def store_data(self, inputs, utility):
+    def store_data(self, utility):
         """
         Keeps track of the recent inputs + errors.
-        TODO: Update this to remember 100 data points?
         """
-        self.input_history.append(inputs)
+        self.input_history.append(np.asarray(self.last_inputs))
         self.utility_history.append(utility)
 
         if len(self.input_history) > self.history_length:
             self.input_history.pop(0)
-            self.utility_history.pop(0) # assumes the histories are of the same length
+            self.utility_history.pop(0)  # assumes the histories are of the same length
+
+    def update(self):
+        """
+        Updates the signalling method, then returns the change in signal importance that each incoming node presents.
+        """
+        # Updates the signalling method.
+        self.update_signal_method()
+
+        # Calculates the new signal importance of the updated signalling method, and returns it.
+        new_signal_importance = self.calculate_signal_importance()
+        signal_importance_change = new_signal_importance - self.last_importance
+        self.last_importance = new_signal_importance.copy()
+
+        return signal_importance_change
 
     def update_signal_method(self):
         """
         Updates the signalling method--in this case, the agent is minimizing the error to the desired output by changing
          the connections that activate the linear unit.
+        Then, returns the change in signal importance that each incoming node presents.
         """
-        # TODO: Why does this regression not return a line with slope 0.5 and intercept 0.5?
+        # Calculates the new signalling method
         slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(np.asarray(self.input_history).reshape(-1),
                                                                              np.asarray(self.utility_history).reshape(-1))
-        self.connections = slope
-        self.bias = intercept
+        self.connections = np.array(slope)
 
     def calculate_signal_importance(self):
         """
@@ -64,8 +86,94 @@ class Agent:
         """
         return abs(self.connections)
 
-    def update_signal_importance(self):
+
+class InputAgent(Agent):
+    """
+    This agent works to send linear inputs into the network. It doesn't ever update, and returns a NONE value for
+    signal importance.
+    """
+    def __init__(self, agent_id, n_connections_in):
+        super.__init__(agent_id, 0)
+
+    def signal(self, input):
+        """Returns the input values"""
+        return input
+
+    def store_data(self, utility):
+        return
+
+    def update(self):
+        return None
+
+
+class LinearSRS(Agent):
+    """
+    This agent uses a random exploration rule to maximize it's perceived utility.
+    """
+
+    def __init__(self, agent_id, n_connections_in):
+        super().__init__(agent_id, n_connections_in)
+
+        # logs the random searches and time to a new search
+        self.time_to_update = 50
+        self.time_since_update = self.time_to_update+1
+        self.recent_update = np.zeros(self.connections.shape)
+        self.expected_utility = -np.inf
+
+    def signal(self, input):
         """
-        Signal importance here is equivalent to the connection strength... so we'll just update the signalling method.
+        Processes a set of inputs into a single signal output. Records this last input
         """
+        self.last_inputs = input
+        return np.sum(self.connections*np.asarray(input))
+
+    def store_data(self, utility):
+        """
+        Keeps track of the recent inputs + errors.
+        """
+        self.input_history.append(np.asarray(self.last_inputs))
+        self.utility_history.append(utility)
+
+    def update(self):
+        """
+        Updates the signalling method, then returns the change in signal importance that each incoming node presents.
+        """
+        # Updates the signalling method.
         self.update_signal_method()
+
+        # Calculates the new signal importance of the updated signalling method, and returns it.
+        signal_importance = self.calculate_signal_importance()
+
+        return signal_importance
+
+    def update_signal_method(self):
+        """
+        Checks whether we've spent enough time evaluating the new connections for utility. If so, updates!
+        """
+        # Increment time, and check if we've spent enough time waiting to evaluate the effectiveness of this update
+        self.time_since_update += 1
+        if self.time_to_update < self.time_since_update:
+
+            average_utility = np.mean(self.utility_history[-self.time_to_update:])
+
+            # If the new utility is worse than expected, then undo the changes
+            if average_utility < self.expected_utility:
+                self.connections -= self.recent_update
+
+            # Makes a new update!
+            self.recent_update = (np.random.rand(len(self.connections))-0.5)/20
+            self.connections += self.recent_update
+
+            # Resets the expected utility
+            self.expected_utility = average_utility
+
+            # Resets the time since update
+            self.time_since_update = 0
+
+    def calculate_signal_importance(self):
+        """
+        Calculates the signal importance as: (signal_sent x connection_str) for each incoming connection x utility
+        """
+        signal_importance = self.input_history[-1] * self.connections * self.utility_history[-1]
+        return signal_importance
+
--- network.py
+++ network.py
@@ -9,53 +9,69 @@
 import numpy as np
 import agents
 
-# TODO: Update this network to handle multiple agents (including backpropagating the signal between agents)
 
-class Network:
+class TelephoneNetwork:
     """
-    Assumes the simplest network is a 1-node linear regression
+    Creates a network of agents strung together in 1d, like a game of telephone.
     """
 
-    def __init__(self):
+    def __init__(self, n_agents=1):
         """
-        Initializes the network, specifying each agent
+        Initializes the network, specifying each agent and which agents connect to one another
         """
-        self.agents = [agents.Agent(1)]
+        # TODO: Let's build in the definitions of an input node. That'll allow us to experiment with mutliple inputs,
+        # TODO: and test whether the errors we're seeing are exclusive to line-line networks.
+        self.agents = [agents.LinearSRS(agent_id=i, n_connections_in=2) for i in range(n_agents)]
+        self.connections = [[i,i+1] for i in range(n_agents-1)]
 
     def predict(self, input):
         """
-        Since this is a one-agent network, we can simply log the one agent's predictions given the input.
-        :param input:
+        Calculates each agent's response to the input by feeding foward the signals from each agent to the next.
         :return:
         """
         output = []
-        for agent in self.agents:
-            output.append(agent.signal(input))
+        agent_inputs = [[1] for agent in self.agents]  # Creates an array of inputs for each agent at a given timestep. Each agent has at least 1 input--a unitary bias term.
+        agent_inputs[0].append(input[0])  # For the first node, append the input.
+
+        # Iterates over agents to propagate the signal from each agent to the next
+        for i, agent in enumerate(self.agents):
+            agent_signal = agent.signal(agent_inputs[i])
+
+            # sends signals to each agent that connects to this one
+            for connection in self.connections:
+                if connection[0] == i:
+                    agent_inputs[connection[1]].append(agent_signal)
+
+            output.append(agent_signal)  # Gathers the last agent's behavior
 
         return output
 
-    def log(self, signals, outcome):
+    def log(self, outcome):
         """
         Logs the true expected outcome, and backpropagates the change in utility.
         """
+        # Sets up the outcome tracker--which records the error / change in utility for each agent.
         agent_outcome = np.zeros(len(self.agents))
-        agent_outcome[-1] = outcome
+        agent_outcome[-1] += outcome  # Sets the last agent's error to the outcome.
 
-        for i in range(len(self.agents)):
-            agent = self.agents[-i]
-            agent.store_data([signals], outcome)
-            agent.update_signal_importance()
-            agent.update_signal_method()
+        # Iterates over agents, from last to first,
+        for i in range(len(self.agents)-1,-1,-1):
+            agent = self.agents[i]
 
+            # Record the outcome for this agent, and associate it with the agent's last inputs.
+            agent.store_data(agent_outcome[agent.id])
+
+            # Update the method this agent uses for signalling based on this new data
+            agent_importance_change = agent.update()
+
+            # Iterates over the connections and propagates the signal reward.
+            # Assumes that the first connections found will be the first signals reported
+            connection_num = 0
+            for connection in self.connections:
+                if connection[1] == agent.id:
+                    # TODO: This currently only works for agents that send a single signal. Fix this to index better (below).
+                    agent_outcome[connection[0]] += agent_importance_change[connection_num+1]
+                    # TODO: Figure out how better to index connections. Sending them forward is easy, because the same
+                    # TODO: signal is sent to all other nodes, but sending utility backwards requires a better structure
+                    connection_num += 1
         return
-
-    def update(self):
-        """
-        Loops through all agents and invokes an update function.
-        """
-        for agent in self.agents:
-            agent.update_signal_method()
-            agent.update_signal_importance()
-
-        return
-
